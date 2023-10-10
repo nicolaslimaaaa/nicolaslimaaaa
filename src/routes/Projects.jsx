e8a50104233aa12ccab9688aad73e0a171c3f18a
Nicolas Lima <nicolaslima1500@gmail.com>
2023-10-10 17:28:36 -0300

    adiciona projetos

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,50 +1,37 @@
-import template from '../assets/template-capa-de-projeto.jpg'
+import UCA from '../assets/projects/UCA.jpeg'
+import Portfolio from '../assets/projects/portfolio.jpeg'
 import { AiOutlineGithub, AiOutlineLink } from "react-icons/ai";
 
 const Projects = () => {
+  const projects = [
+    {
+      title: 'Portfólio',
+      deploy: 'https://nicolaslimaaaa.vercel.app/',
+      repository:'https://github.com/nicolaslimaaaa/nicolaslimaaaa',
+      img: Portfolio,
+    },
+    {
+      title: 'UCA!',
+      deploy: 'https://trybewallet-vert-sigma.vercel.app/',
+      repository:'https://github.com/nicolaslimaaaa/Trybewallet',
+      img: UCA,
+    },
+  ];
+
   return (
-    <section className='flex justify-center items-center gap-y-10 gap-x-5 flex-wrap md:mt-28 2xl:mt-0 mx-16 min-h-screen'>
-      <div className='flex flex-col w-60 gap-y-4 text-xl'>
-        <h4 className='text-text-color text-center'>Projeto 1</h4>
-        <img src={template} alt="Imagem do projeto" className='rounded-2xl border border-main-color'/>
-        <div className='flex items-center justify-evenly text-text-color'>
-        <a href="" target='_blank'><AiOutlineLink /></a>
-        <a href="" target='_blank'><AiOutlineGithub /></a>
-        </div>
-      </div>
-      <div className='flex flex-col w-60 gap-y-4 text-xl'>
-        <h4 className='text-text-color text-center'>Projeto 1</h4>
-        <img src={template} alt="Imagem do projeto" className='rounded-2xl border border-main-color'/>
-        <div className='flex items-center justify-evenly text-text-color'>
-        <a href="" target='_blank'><AiOutlineLink /></a>
-        <a href="" target='_blank'><AiOutlineGithub /></a>
-        </div>
-      </div>
-      <div className='flex flex-col w-60 gap-y-4 text-xl'>
-        <h4 className='text-text-color text-center'>Projeto 1</h4>
-        <img src={template} alt="Imagem do projeto" className='rounded-2xl border border-main-color'/>
-        <div className='flex items-center justify-evenly text-text-color'>
-        <a href="" target='_blank'><AiOutlineLink /></a>
-        <a href="" target='_blank'><AiOutlineGithub /></a>
-        </div>
-      </div>
-      <div className='flex flex-col w-60 gap-y-4 text-xl'>
-        <h4 className='text-text-color text-center'>Projeto 1</h4>
-        <img src={template} alt="Imagem do projeto" className='rounded-2xl border border-main-color'/>
-        <div className='flex items-center justify-evenly text-text-color'>
-        <a href="" target='_blank'><AiOutlineLink /></a>
-        <a href="" target='_blank'><AiOutlineGithub /></a>
-        </div>
-      </div>
-      <div className='flex flex-col w-60 gap-y-4 text-xl'>
-        <h4 className='text-text-color text-center'>Projeto 1</h4>
-        <img src={template} alt="Imagem do projeto" className='rounded-2xl border border-main-color'/>
-        <div className='flex items-center justify-evenly text-text-color'>
-        <a href="" target='_blank'><AiOutlineLink /></a>
-        <a href="" target='_blank'><AiOutlineGithub /></a>
-        </div>
-      </div>
-    
+    <section className='flex justify-center items-center gap-x-5 flex-wrap md:mt-28 lg:mt-0 mx-16 min-h-screen'>
+      {
+        projects.map(({ title, deploy, repository, img }, index) => (
+          <div key={index} className='flex flex-col w-4/5 md:w-1/2 lg:w-1/3 gap-y-6 text-xl'>
+            <h4 className='text-main-color text-center'>{title}</h4>
+            <img src={img} alt="Imagem do projeto" className='rounded-2xl border border-mono-color'/>
+            <div className='flex items-center justify-evenly text-text-color'>
+              <a href={deploy} target='_blank' rel="noreferrer"><AiOutlineLink /></a>
+              <a href={repository} target='_blank' rel="noreferrer"><AiOutlineGithub /></a>
+            </div>
+          </div>
+        ))
+      }
     </section>
   )
 }
